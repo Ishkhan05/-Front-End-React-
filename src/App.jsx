3a94305e50896ad166b1820b35b94b99126ca6eb
Ishkhan05 <g.i.h.a.s@bk.ru>
2025-03-05 21:16:15 +0400

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import Content from './Components/Content/Content'
-import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <div className='App'>
-      <Header/>
       <Content/>
     </div>
   )
--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -4,28 +4,30 @@ import "./Content.css"
 
 export default function Content() {
   const [posts, setPosts] = useState()
-  const [bigImage, setBigImage] = useState(false)
-  const [bigImageLink, setBigImageLink] = useState()
+  const [postInfoModal, setPostInfoModal] = useState(false)
+  const [currentPost, setCurrentPost] = useState()
   useEffect(() => {
     axios("https://cloud.codesupply.co/endpoint/react/data.json")
     .then(res=>setPosts(res.data))
   }, [])
-  const image2x = (link)=>{
-    setBigImage(!bigImage)
-    setBigImageLink(link)
+  const postInfo = (title)=>{
+    setPostInfoModal(!postInfoModal)
+    setCurrentPost(posts.find((elem)=>elem.title == title))
   }
   const modalClose = (e) =>{
-    if(e.target.tagName!=="IMG"){
-      setBigImage(false)
-    }
+    e.target.className=="modal" ? setPostInfoModal(false): null 
   }
   return (
     <div className='Content'>
       {
         posts?.map(post=>{
           return(
-            <div key={post?.title} className='post'>
-              <img src={post.img} alt="" onClick={()=>image2x(post.img_2x)}/>
+            <div key={post?.title} className='post' onClick={()=>postInfo(post.title)}>
+              <picture>
+                <source media="(min-width:1080px)" srcSet={post.img_2x} />
+                <source media="(min-width:465px)" srcSet={post.img} />
+                <img src={post.img_2x} alt={post.name} style={{width: "auto"}} />
+              </picture>
               <p>{post.tags}</p>
               <h2>{post.title}</h2>
               <div>
@@ -38,9 +40,25 @@ export default function Content() {
           )
         })
       }
-      {bigImage? (<div className='bigImage'  onClick={modalClose}>
-        <img src={bigImageLink} alt=""/>
-      </div>) :null}
+      {postInfoModal? (
+        <div className='modal' onClick={modalClose}>
+          <div className='postInfo'>
+            <picture>
+              <source media="(min-width:1080px)" srcSet={currentPost.img_2x} />
+              <source media="(min-width:465px)" srcSet={currentPost.img} />
+              <img src={currentPost.img_2x} alt={currentPost.name} style={{ width: "auto" }} />
+            </picture>
+            <p>{currentPost.tags}</p>
+            <h2>{currentPost.title}</h2>
+            <div>
+              <p>{currentPost.autor}</p>
+              <p>{currentPost.date}</p>
+              <p>{currentPost.views}</p>
+            </div>
+            <p>{currentPost.text}</p>
+          </div>
+        </div>) 
+        :null}
     </div>
   )
 }
